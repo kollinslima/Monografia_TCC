12300b846ca0ce37697c916d7533d7dd8f31645e
kollinslima <kollins.lima@usp.br>
2018-08-01 16:41:45 -0300

    Resultados 1

--- Code/Blink.c
+++ Code/Blink.c
@@ -1,24 +1,26 @@
 /*
   Blink
 
-  Turns an LED on for one microsecond, then off for one microsecond, repeatedly.
+  Turns an LED on for one millisecond, then off for one millisecond, repeatedly.
 
-  This code was modified by Kollins Lima (May 25, 2018).
+  This code was modified by Kollins Lima (August 01, 2018).
 
   The original code is available at:
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
+int output_pin = LED_BUILTIN;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(output_pin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delayMicroseconds(1);             // wait for a microsecond
+  delay(1);                         // wait for a millisecond
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delayMicroseconds(1);             // wait for a microsecond
+  delay(1);                         // wait for a millisecond
 }
