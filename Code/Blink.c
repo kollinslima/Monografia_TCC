66a50531e8f7a2cbeeb6d2577b3ffb157f9ab196
kollinslima <kollins.lima@usp.br>
2018-08-07 16:39:08 -0300

    Resultados 7

--- Code/Blink.c
+++ Code/Blink.c
@@ -18,8 +18,8 @@ void setup() {
 }
 // the loop function runs over and over again forever
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(output_pin, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(1);                         // wait for a millisecond
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    digitalWrite(output_pin, LOW);   // turn the LED off by making the voltage LOW
     delay(1);                         // wait for a millisecond
 }
